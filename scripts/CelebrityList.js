7678d3003d8b5272299d85d6481a56d838d7d8fc
Eric Martinez <ericmartinez1007@gmail.com>
2026-02-25 09:50:19 -0600

    Added click event for Celebrities

--- scripts/CelebrityList.js
+++ scripts/CelebrityList.js
@@ -2,12 +2,32 @@ import { getCelebrities } from "./database.js"
 
 const celebrities = getCelebrities()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const celebrityTarget = clickEvent.target
+
+        if (celebrityTarget.dataset.type === "celebrity"){
+
+            const celebrityId = parseFloat(celebrityTarget.dataset.celebrityid)
+
+            for (const celebrity of celebrities){
+                if (celebrityId === celebrity.id) {
+                    window.alert(`${celebrity.name} is a ${celebrity.sport} star`)
+                }
+                
+            } 
+            
+        }
+    }
+)
+
 export const Celebrities = () => {
     let html = "<ol>"
 
     for (const celebrity of celebrities) {
         html += `<li 
-                    data-id="${celebrity.id}" 
+                    data-celebrityid="${celebrity.id}" 
                     data-type="celebrity"
                     data-sport="${celebrity.sport}"
                     id="star--${celebrity.id}">
